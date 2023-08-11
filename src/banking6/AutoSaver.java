3f5c9e6a9ee4f32337505cf13cacfa66280ef957
hyeji_1610 <hyeji_1610@naver.com>
2023-08-11 22:49:00 +0900

    0811

--- src/banking6/AutoSaver.java
+++ src/banking6/AutoSaver.java
@@ -9,6 +9,12 @@ public class AutoSaver extends Thread {
 		while(true) {
 			try {
 				sleep(5000);
+				
+				for(Account acc : accountset) {
+					out.printf("%s\n",acc.toString());
+				}
+				
+				
 				System.out.println("자동저장 됐습니다.");
 			}
 			catch(InterruptedException e) {
